ade8cc072c7aa3f592759383c9db23422a58b72e
BuildBoy12 <buildboy12345@gmail.com>
2020-07-25 15:58:54 -0400

    Update event method
    Changed to just cancel damage and do my own since ev.Damage didnt cooperate.

--- DamageFalloff/EventHandlers.cs
+++ DamageFalloff/EventHandlers.cs
@@ -1,3 +1,4 @@
+using Exiled.API.Features;
 using Exiled.Events.EventArgs;
 
 namespace DamageFalloff
@@ -9,7 +10,20 @@ namespace DamageFalloff
 
 		public void OnLateShoot(ShotEventArgs ev)
 		{
-			ev.Damage = plugin.DistanceLogic.DoMath(ev.Damage, ev.Distance, ev.Shooter.ReferenceHub);
+			Player target = Player.Get(ev.Target);
+			ev.CanHurt = false;
+			target.ReferenceHub.playerStats.HurtPlayer(new PlayerStats.HitInfo(plugin.DistanceLogic.DoMath(ev.Damage, ev.Distance, ev.Shooter.ReferenceHub), ev.Shooter.Nickname, WeapontoDamageType(ev.Shooter.ReferenceHub.inventory.curItem), ev.Shooter.Id), target.GameObject, false);
 		}
+
+		private DamageTypes.DamageType WeapontoDamageType(ItemType item)
+        {
+			if (item == ItemType.GunCOM15) return DamageTypes.Com15;
+			else if (item == ItemType.GunE11SR) return DamageTypes.E11StandardRifle;
+			else if (item == ItemType.GunLogicer) return DamageTypes.Logicer;
+			else if (item == ItemType.GunMP7) return DamageTypes.Mp7;
+			else if (item == ItemType.GunProject90) return DamageTypes.P90;
+			else if (item == ItemType.GunUSP) return DamageTypes.Usp;
+			return DamageTypes.Wall;
+        }
 	}
 }
